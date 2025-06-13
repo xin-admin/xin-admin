4502d5b5d7d1984ab724761afcea3ca4c366ec0d
liu <2302563948@qq.com>
2025-06-13 14:50:10 +0800

    feat: 完善顶栏切换

--- src/layout/HeaderRender.tsx
+++ src/layout/HeaderRender.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Breadcrumb, Button, ConfigProvider, Layout} from "antd";
+import {Breadcrumb, Button, ConfigProvider, Layout, Menu} from "antd";
 import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
 import {useGlobalStore} from "@/stores";
 import HeaderLeftRender from "@/layout/HeaderLeftRender";
@@ -11,6 +11,8 @@ const HeaderRender: React.FC = () => {
     const setCollapsed = useGlobalStore(state => state.setCollapsed);
     const themeConfig = useGlobalStore(state => state.themeConfig);
     const breadcrumbItems = useGlobalStore(state => state.breadcrumbItems);
+    const layout = useGlobalStore(state => state.layout);
+    const siderMenus = useGlobalStore(state => state.siderMenus);
 
     return (
         <ConfigProvider
@@ -22,20 +24,30 @@ const HeaderRender: React.FC = () => {
         >
             <Header className={"flex sticky z-1 top-0 backdrop-blur-xs"}>
                 <HeaderLeftRender/>
-                <div className={"flex items-center flex-1"}>
-                    <Button
-                        type={'text'}
-                        className={'text-[16px] mr-2'}
-                        onClick={() => setCollapsed(!collapsed)}
-                    >
-                        { collapsed ?
-                            <MenuUnfoldOutlined  />
-                            :
-                            <MenuFoldOutlined/>
-                        }
-                    </Button>
-                    <Breadcrumb items={breadcrumbItems}/>
-                </div>
+                { layout !== 'top' ?
+                    <div className={"flex items-center flex-1"}>
+                        <Button
+                            type={'text'}
+                            className={'text-[16px] mr-2'}
+                            onClick={() => setCollapsed(!collapsed)}
+                        >
+                            { collapsed ?
+                                <MenuUnfoldOutlined  />
+                                :
+                                <MenuFoldOutlined/>
+                            }
+                        </Button>
+                        <Breadcrumb items={breadcrumbItems}/>
+                    </div>
+                    :
+                    <div className={"flex-1"}>
+                        <Menu
+                            defaultSelectedKeys={['1']}
+                            mode="horizontal"
+                            items={siderMenus}
+                        />
+                    </div>
+                }
                 <HeaderRightRender/>
             </Header>
         </ConfigProvider>
