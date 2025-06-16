44e39b62f6bd89a7213c179e76ecc2bb1ebff44f
liu <2302563948@qq.com>
2025-06-16 18:01:33 +0800

    fix: 移除不用的import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import '@ant-design/v5-patch-for-react-19';
-import './index.css';
 
 const root = document.getElementById("root");
 
