63682c39565753ddf9d9b5a46908c0ca7c7d27ba
liu <2302563948@qq.com>
2025-06-24 10:48:04 +0800

    feat: 更新顶部工具栏

--- src/layout/HeaderRightRender.tsx
+++ src/layout/HeaderRightRender.tsx
@@ -1,10 +1,22 @@
-import {Avatar, Button, Dropdown, type MenuProps, Space, message} from "antd";
-import {SettingOutlined,TranslationOutlined, FullscreenExitOutlined, FullscreenOutlined} from "@ant-design/icons";
+import {Avatar, Button, Dropdown, type MenuProps, Modal, Space, message, Input, Empty, theme} from "antd";
+import {
+    SettingOutlined,
+    TranslationOutlined,
+    FullscreenExitOutlined,
+    FullscreenOutlined,
+    SearchOutlined,
+    ArrowUpOutlined,
+    EnterOutlined,
+    ArrowDownOutlined,
+    GithubOutlined,
+    HomeOutlined
+} from "@ant-design/icons";
 import {useGlobalStore} from "@/stores";
 import {useTranslation} from "react-i18next";
 import useAuthStore from "@/stores/user";
 import {useNavigate} from "react-router";
 import {useState} from "react";
+const { useToken } = theme;
 
 const HeaderLeftRender = () => {
     const navigate = useNavigate()
@@ -14,6 +26,8 @@ const HeaderLeftRender = () => {
     const logout = useAuthStore(state => state.logout)
     const { i18n } = useTranslation();
     const [fullscreen, setFullscreen] = useState<boolean>(false);
+    const [searchOpen, setSearch] = useState<boolean>(false);
+    const { token } = useToken();
     const onFullscreenClick = () => {
         /* 获取 documentElement (<html>) 以全屏显示页面 */
         const elem = document.documentElement;
@@ -63,28 +77,53 @@ const HeaderLeftRender = () => {
     ]
 
     return (
-        <Space>
-            <Button
-                onClick={onFullscreenClick}
-                icon={ fullscreen ? <FullscreenExitOutlined/> : <FullscreenOutlined/>}
-                size={"large"}
-                type={'text'}
-            />
-            <Dropdown menu={{ items: localesItems }}>
-                <Button icon={<TranslationOutlined />} size={"large"} type={'text'}/>
-            </Dropdown>
-            <Button onClick={() => setThemeDrawer(!themeDrawer)} icon={<SettingOutlined/>} size={"large"} type={'text'}/>
-            { userInfo ?
-                <Dropdown menu={{ items: userItems }}>
-                    <Button size={"large"} type={'text'}>
-                        <div>{ userInfo.nickname || userInfo.username }</div>
-                        <Avatar src={userInfo.avatar_url} size={'small'}/>
-                    </Button>
+        <>
+            <Modal
+                closable={false}
+                open={searchOpen}
+                maskClosable
+                footer={null}
+                style={{ top: 40 }}
+                styles={{content: {padding: 0, width: 600, maxHeight: '80vh'}}}
+                onCancel={() => setSearch(false)}
+            >
+                <div className={'p-5'}>
+                    <Input size="large" placeholder="Please enter the search content" prefix={<SearchOutlined />} />
+                    <div className={'mt-5'}>
+                        <Empty />
+                    </div>
+                </div>
+                <Space className={'mt-5 flex align-center pl-5 pr-5 pt-2.5 pb-2.5'} style={{ borderTop: '1px solid ' + token.colorBorder }}>
+                    <EnterOutlined /> <span className={'mr-4'}>Confirm</span>
+                    <ArrowUpOutlined /><ArrowDownOutlined /> <span className={'mr-4'}>Switch</span>
+                    <span className={'mr-2'}>Close</span>
+                </Space>
+            </Modal>
+            <Space>
+                <Button icon={<HomeOutlined />} size={'large'} type={'text'} onClick={() => window.open('https://xin-admin.com')}/>
+                <Button icon={<GithubOutlined />} size={'large'} type={'text'} onClick={() => window.open('https://github.com/xin-admin/xin-admin-ui')}/>
+                <Button icon={<SearchOutlined/>} size={'large'} type={'text'} onClick={() => setSearch(true)} />
+                <Button
+                    onClick={onFullscreenClick}
+                    icon={ fullscreen ? <FullscreenExitOutlined/> : <FullscreenOutlined/>}
+                    size={"large"}
+                    type={'text'}
+                />
+                <Dropdown menu={{ items: localesItems }}>
+                    <Button icon={<TranslationOutlined />} size={"large"} type={'text'}/>
                 </Dropdown>
-                : null
-            }
-
-        </Space>
+                <Button onClick={() => setThemeDrawer(!themeDrawer)} icon={<SettingOutlined/>} size={"large"} type={'text'}/>
+                { userInfo ?
+                    <Dropdown menu={{ items: userItems }}>
+                        <Button size={"large"} type={'text'}>
+                            <div>{ userInfo.nickname || userInfo.username }</div>
+                            <Avatar src={userInfo.avatar_url} size={'small'}/>
+                        </Button>
+                    </Dropdown>
+                    : null
+                }
+            </Space>
+        </>
     )
 }
 
