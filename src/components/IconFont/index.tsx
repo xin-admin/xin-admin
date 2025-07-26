5dc6baaabca53e372620f4beb5135b1f451308db
liu <2302563948@qq.com>
2025-07-26 23:00:12 +0800

    feat: 新增图标选择

--- src/components/IconFont/index.tsx
+++ src/components/IconFont/index.tsx
@@ -1,13 +1,12 @@
 import { createFromIconfontCN, ExclamationOutlined } from '@ant-design/icons';
 import * as AntdIcons from '@ant-design/icons';
 import React from 'react';
-import { categories } from './fields'
+import {categories, oauthScriptUrl} from '@/utils/iconFields.ts'
 
 const allIcons: any = AntdIcons;
 
 const OtherIcons = createFromIconfontCN({
-  // 以下是默认值，也可以按需要指定
-  scriptUrl: '//at.alicdn.com/t/c/font_4413039_6ow46w95lhw.js',
+  scriptUrl: oauthScriptUrl,
 });
 
 const IconFont = (props: {name?: string}) => {
